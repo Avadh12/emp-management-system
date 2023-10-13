0f4b94c2e3fbebc96a0c5e5e50017cf02b0dbbad
avadhsharma010 <147719208+avadhsharma010@users.noreply.github.com>
2023-10-13 14:28:54 +0530

    Update TaskCreateRequest.java
    add element

--- src/main/java/com/example/Employee_Management/model/TaskCreateRequest.java
+++ src/main/java/com/example/Employee_Management/model/TaskCreateRequest.java
@@ -10,4 +10,19 @@ import lombok.*;
 public class TaskCreateRequest {
     private Integer projectId;
     private String taskDescription;
+    public class PrintEvenCharacters {
+    public static void main(String[] args) {
+        String input = "I love my country";
+        printEvenCharacters(input);
+    }
+
+    private static void printEvenCharacters(String input) {
+        for (int i = 0; i < input.length(); i += 2) {
+            char character = input.charAt(i);
+            if (character != ' ') {
+                System.out.print(character);
+            }
+        }
+    }
+}
 }
